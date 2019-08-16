65b755547cdb111a648f3f55f7625bb891d84281
Sarah Guichard <sarah.gcd1@gmail.com>
2019-08-16 12:48:16 +0200

    removed validation and ! in seeds

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,5 @@
 class Ingredient < ApplicationRecord
-  INGREDIENTS = Ingredient.all
   has_many :doses
   has_many :cocktails, through: :doses
-  validates :name, presence: true, uniqueness: true, inclusion: { in: INGREDIENTS }
+  validates :name, presence: true, uniqueness: true
 end
